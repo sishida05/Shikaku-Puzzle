0e471b6fd774d20b4806a9fd9f97258d9f5db943
sishida05 <30080170@cc.peralta.edu>
2026-04-15 18:32:21 +0000

    Added: Counter class, tests, and CMake setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// src/main.cpp
+#include "Counter.h"
+#include <iostream>
+ 
+int main() {
+    Counter c;
+    c.increment();
+    c.increment();
+    c.increment();
+    std::cout << "Counter value: " << c.getValue() << std::endl;
+    return 0;
+}
