8ba5844fe2ce4fb4327accf8e6cb7f3a81643e61
Sean Ishida <30080170@cc.peralta.edu>
2026-05-23 20:39:06 -0700

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,25 @@ using namespace std;
 int main() {
 
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Test");
-    while (window.isOpen()) {}
+    // Create a 200x200 window titled "SFML works!"
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
 
-    // // Create a 200x200 window titled "SFML works!"
-    // sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    // Create a green circle shape with a radius of 100
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // // Create a green circle shape with a radius of 100
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
+    // Main application loop
+    while (window.isOpen()) {
 
-    // // Main application loop
-    // while (window.isOpen()) {
+        // Clear the window with a default color (black)
+        window.clear();
 
-    //     // Clear the window with a default color (black)
-    //     window.clear();
+        // Draw the circle shape
+        window.draw(shape);
 
-    //     // Draw the circle shape
-    //     window.draw(shape);
-
-    //     // Display the contents of the window
-    //     window.display();
-    // }
-
+        // Display the contents of the window
+        window.display();
+    }
 
     return 0;
 }
